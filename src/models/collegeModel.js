60b19b695b05aa20deb036547f3c3c97ec48f2b2
Tirthankar Roy <roytirthankar2@gmail.com>
2023-01-09 13:44:35 +0530

    schemaAdded2

--- src/models/collegeModel.js
+++ src/models/collegeModel.js
@@ -1,40 +1,26 @@
 const mongoose = require("mongoose")
 
 const collegeSchema = new mongoose.Schema({
-    title: {
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+    },
+    fullName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    logoLink: {
         type: String,
         required: true
     },
-    body: {
-        type: String,
-        required: true
-    },
-    authorId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Author'
-    },
-    tags: [String],
-    category: {
-        type: String,
-        required: true
-    },
-    subcategory: [String],
     isDeleted: {
         type: Boolean,
         default: false
-    },
-    isPublished: {
-        type: Boolean,
-        default: false
-    },
-    deletedAt: {
-        type: Date,
-        default: null
-    },
-    publishedAt: {
-        type: Date,
-        default: null
     }
 }, { timestamps: true })
 
-module.exports = mongoose.model('college', collegeSchema)
+module.exports = mongoose.model('College', collegeSchema)
