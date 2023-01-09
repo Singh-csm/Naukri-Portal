1d3d8ed5ead646a0fbcda45556560a69cc134fae
Tirthankar Roy <roytirthankar2@gmail.com>
2023-01-09 13:33:21 +0530

    schemaAdded

---
+++ src/models/internModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose")
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const internSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true
+        },
+        mobile: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        collegeId: {
+            type: ObjectId,
+            ref: "College",
+            required: true
+        },
+        isDeleted: {
+            type: Boolean,
+            default: false
+        },
+    }, {timestamps: true}
+);
+
+module.exports = new mongoose.model("Intern", internSchema)
