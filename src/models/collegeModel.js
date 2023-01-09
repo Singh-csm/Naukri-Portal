8bcd4eb4b2f5d862595e83bde1ebe7011bc4ee16
Singh-csm <csm8107@gmail.com>
2023-01-09 12:38:28 +0530

    file-added

---
+++ src/models/collegeModel.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose")
+
+const collegeSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    authorId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Author'
+    },
+    tags: [String],
+    category: {
+        type: String,
+        required: true
+    },
+    subcategory: [String],
+    isDeleted: {
+        type: Boolean,
+        default: false
+    },
+    isPublished: {
+        type: Boolean,
+        default: false
+    },
+    deletedAt: {
+        type: Date,
+        default: null
+    },
+    publishedAt: {
+        type: Date,
+        default: null
+    }
+}, { timestamps: true })
+
+module.exports = mongoose.model('college', collegeSchema)
